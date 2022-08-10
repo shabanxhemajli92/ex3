65f3433597ec850f0c49b5a5862dbfdfd4b602ee
Shaban <shabanxhemajli@live.com>
2022-08-10 15:56:41 +0200

    ababababababab

--- exercise (4).py
+++ exercise (4).py
@@ -5,3 +5,13 @@ for i in range(0,3):
         print("even number")
     else:
         print("it is odd")     
+
+# Task 2
+n=input("How many arguments you want to use: ")
+start=input("Enter the number needed to start: ")
+stop=input("This is the point to where everything stops!")
+step=input("How many numbers do you want to count up in a step: ")
+for i in range(start,stop,step):
+    print(i)
+              
+        
