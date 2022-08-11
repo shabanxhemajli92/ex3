e6cab1d1f280f92cb9b051ecae8cb9c18a23b3fe
Shaban <shabanxhemajli@live.com>
2022-08-11 10:08:52 +0200

    ffff

--- exercise (4).py
+++ exercise (4).py
@@ -1,17 +1,31 @@
-# Task 1
+ # Task 1
 for i in range(0,3):
-    n= input("give me a number:")
-    if int(n) % 2==0:
-        print("even number")
-    else:
+     n= input("give me a number:")
+     if int(n) % 2==0:
+         print("even number")
+     else:
         print("it is odd")     
 
 # Task 2
 n=input("How many arguments you want to use: ")
-start=input("Enter the number needed to start: ")
-stop=input("This is the point to where everything stops!")
-step=input("How many numbers do you want to count up in a step: ")
+n=int(n)
+if n>=3:
+    start=input("Insert the number needed to start: ")
+    stop=input("This is the point to where everything stops!")
+    step=input("How many numbers do you want to count up in a step: ")
+   #this will check the number of arguments,this will check for the 3 arguments(sss)
+elif n==2:
+    step=1
+    start=input("Insert the number needed to start: ")
+    stop=input("This is the point to where everything stops!")    
+    #this will only check for 2 arguments
+else:
+    start=1
+    step=1
+    stop=input("This is the point to where everything stops!")
+    #this will only check for 1 argument    
+start=int(start)
+stop=int(stop)
+step=int(step)
 for i in range(start,stop,step):
-    print(i)
-              
-        
+   print(i)
