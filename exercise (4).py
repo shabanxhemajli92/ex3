dbbd0fd8a7e8e269a44b3597ec5bce67093bee20
Shaban <shabanxhemajli@live.com>
2022-08-12 09:18:58 +0200

    exercise 4

--- exercise (4).py
+++ exercise (4).py
@@ -1,7 +1,4 @@
 ''' # Task 1
-from argparse import RawTextHelpFormatter
-
-
 for i in range(0,3):
      n= input("give me a number:")
      if int(n) % 2==0:
@@ -39,7 +36,7 @@ for j in range(2,v+1,1):
 #this will devide the num by two and will stop at the number above it , dividing it by every single number    
     if v % j == 0:
         print(j)'''
-    #Task 4
+'''#Task 4
 v=int(input("Insert a prime number :"))
 s=True
 for j in range(2,v):
@@ -48,6 +45,24 @@ for j in range(2,v):
 if s == True:         
     print(v,"is a prime number")
 else:
-    print(v,"is not a prime number")                        
+    print(v,"is not a prime number")'''                        
 
+'''#Task 5
+for FiZZBuzz in range (100):
+    if FiZZBuzz % 3 == 0 and FiZZBuzz % 5 == 0:
+        print("FiZZBuzz")
 
+    elif FiZZBuzz % 3 == 0:
+        print("Fizz")
+    elif FiZZBuzz % 5 == 0:
+              print("Buzz")
+
+    else:
+          print(FiZZBuzz)'''
+ 
+ # Task 6
+lower=int(input("The lowest number on the range: "))
+upper=int(input("The highest number in the range: "))
+for i in range (lower ,upper):
+    if i%7==0 and i%5==0:
+       print(i) 
